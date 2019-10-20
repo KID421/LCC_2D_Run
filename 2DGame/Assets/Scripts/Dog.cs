cea5b600718cbbabe3100227599df20b52dcaeb7
KID <50666207+KID421@users.noreply.github.com>
2019-10-20 10:41:02 +0800

    建立 C# 與定義欄位

---
+++ 2DGame/Assets/Scripts/Dog.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class Dog : MonoBehaviour
+{
+    // 欄位 field (變數)
+    // 欄位類型 欄位名稱 (指定 值) 結束
+    int jumpCount = 2;
+    int jump = 100;
+    float speed = 10.5f;
+    bool isGround;              // true/false
+    string dogName = "KID";
+}
