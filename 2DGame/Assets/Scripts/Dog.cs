5238d219599cd3f8bc5227e87d6cc7ae8870b0e9
KID <50666207+KID421@users.noreply.github.com>
2019-10-20 11:12:32 +0800

    欄位修飾詞與屬性

--- 2DGame/Assets/Scripts/Dog.cs
+++ 2DGame/Assets/Scripts/Dog.cs
@@ -3,10 +3,16 @@
 public class Dog : MonoBehaviour
 {
     // 欄位 field (變數)
-    // 欄位類型 欄位名稱 (指定 值) 結束
-    int jumpCount = 2;
-    int jump = 100;
-    float speed = 10.5f;
-    bool isGround;              // true/false
-    string dogName = "KID";
+    // 修飾詞 欄位類型 欄位名稱 (指定 值) 結束
+    // private 私人(不顯示) public 公開(顯示)
+    [Header("跳躍次數"), Range(1, 10)]
+    public int jumpCount = 2;
+    [Header("跳躍高度")]
+    public int jump = 200;
+    [Header("移動速度"), Range(1.5f, 15.5f)]
+    public float speed = 10.5f;
+    [Header("是否在地板上"), Tooltip("用來判定角色有沒有站在地板上。")]
+    public bool isGround;                   // true 是、false 否
+    [Header("角色名稱")]
+    public string characterName = "KID";
 }
