36bb812b1c29640dcf3eca2434881140f45b472c
KID <50666207+KID421@users.noreply.github.com>
2019-11-03 11:16:42 +0800

    碰撞事件與判定是否為地板

--- 2DGame/Assets/Scripts/Dog.cs
+++ 2DGame/Assets/Scripts/Dog.cs
@@ -10,7 +10,7 @@ public class Dog : MonoBehaviour
     public int jumpCount = 2;
     [Header("跳躍高度")]
     public int jump = 200;
-    [Header("移動速度"), Range(1.5f, 15.5f)]
+    [Header("移動速度"), Range(0, 15.5f)]
     public float speed = 10.5f;
     [Header("是否在地板上"), Tooltip("用來判定角色有沒有站在地板上。")]
     public bool isGround;                   // true 是、false 否
@@ -22,6 +22,7 @@ public class Dog : MonoBehaviour
     public CapsuleCollider2D cc2d;
     #endregion
 
+    #region 事件
     // 初始事件：遊戲開始執行一次
     private void Start()
     {
@@ -37,6 +38,18 @@ public class Dog : MonoBehaviour
         MoveCamera();
     }
 
+    // 碰撞事件：當物件碰撞開始時執行一次
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        // 如果 碰到 物件 名稱 等於 "地板"
+        if (collision.gameObject.name == "地板")
+        {
+            isGround = true;
+        }
+    }
+    #endregion
+
+    #region 方法
     /// <summary>
     /// 狗移動方法。
     /// </summary>
@@ -86,4 +99,5 @@ public class Dog : MonoBehaviour
         cc2d.offset = new Vector2(-0.1f, -0.25f);
         cc2d.size = new Vector2(0.95f, 2.5f);
     }
+    #endregion
 }
