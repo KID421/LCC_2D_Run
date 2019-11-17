bd62aaa281c4053c2002b853decbe273822f33e8
KID <50666207+KID421@users.noreply.github.com>
2019-11-17 10:44:14 +0800

    實作重玩與離開遊戲方法

--- 2DGame/Assets/Scripts/GameManager.cs
+++ 2DGame/Assets/Scripts/GameManager.cs
@@ -1,6 +1,17 @@
 ﻿using UnityEngine;
+using UnityEngine.SceneManagement;  // 引用 場景管理 API
 
 public class GameManager : MonoBehaviour
 {
+    public void Replay()
+    {
+        // 場景管理.載入場景("場景名稱")
+        SceneManager.LoadScene("關卡1");
+    }
 
+    public void Quit()
+    {
+        // 應用程式.離開
+        Application.Quit();
+    }
 }
