3357c40ad66645dc104acba944ae82ad48c7edf8
KID <50666207+KID421@users.noreply.github.com>
2019-11-17 10:55:59 +0800

    重玩方法添加參數並設定按鈕

--- 2DGame/Assets/Scripts/GameManager.cs
+++ 2DGame/Assets/Scripts/GameManager.cs
@@ -3,10 +3,10 @@ using UnityEngine.SceneManagement;  // 引用 場景管理 API
 
 public class GameManager : MonoBehaviour
 {
-    public void Replay()
+    public void Replay(string scene)
     {
         // 場景管理.載入場景("場景名稱")
-        SceneManager.LoadScene("關卡1");
+        SceneManager.LoadScene(scene);
     }
 
     public void Quit()
